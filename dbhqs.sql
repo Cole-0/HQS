3bec6f2a5b855341bf2d8f1077652f02beb67c80
Justine Nicole Daquioag <156783594+Cole-0@users.noreply.github.com>
2025-04-14 22:01:44 +0800

    Add files via upload

---
+++ dbhqs.sql
@@ -0,0 +1,75 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 14, 2025 at 04:01 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `dbhqs`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `appointments`
+--
+-- Error reading structure for table dbhqs.appointments: #1932 - Table &#039;dbhqs.appointments&#039; doesn&#039;t exist in engine
+-- Error reading data for table dbhqs.appointments: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `dbhqs`.`appointments`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `consultations`
+--
+-- Error reading structure for table dbhqs.consultations: #1932 - Table &#039;dbhqs.consultations&#039; doesn&#039;t exist in engine
+-- Error reading data for table dbhqs.consultations: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `dbhqs`.`consultations`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `departments`
+--
+-- Error reading structure for table dbhqs.departments: #1932 - Table &#039;dbhqs.departments&#039; doesn&#039;t exist in engine
+-- Error reading data for table dbhqs.departments: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `dbhqs`.`departments`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `queues`
+--
+-- Error reading structure for table dbhqs.queues: #1932 - Table &#039;dbhqs.queues&#039; doesn&#039;t exist in engine
+-- Error reading data for table dbhqs.queues: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `dbhqs`.`queues`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `services`
+--
+-- Error reading structure for table dbhqs.services: #1932 - Table &#039;dbhqs.services&#039; doesn&#039;t exist in engine
+-- Error reading data for table dbhqs.services: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `dbhqs`.`services`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+-- Error reading structure for table dbhqs.users: #1932 - Table &#039;dbhqs.users&#039; doesn&#039;t exist in engine
+-- Error reading data for table dbhqs.users: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `dbhqs`.`users`&#039; at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
